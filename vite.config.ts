85ce71d812ee1d4f10e7591404c4d40ff631a2ea
olegdurov <olegdurov@outlook.de>
2026-01-17 12:23:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nexild/',
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+  base: '/nexild/',
   resolve: {
     alias: {
       // Alias @ to the src directory
